8162aa38dfc694c5a6c018de2d38fad2fc762381
Anton Chuev <anton.chuev@applitools.com>
2018-02-28 19:07:08 +0200

    Add bridging header to test target on swift

---
+++ HelloWorldUITestsSwift/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  HelloWorldUITestsSwift
+//
+//  Created by Anton Chuev on 2/28/18.
+//  Copyright © 2018 Applitools. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
